b90d33340ef4e06ed0c7082ea8a4746e7e445d8d
fadeache <1424834523@qq.com>
2023-03-31 13:12:49 +0800

    fix: 主题色及base配置

--- vite.config.js
+++ vite.config.js
@@ -5,19 +5,9 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   return {
-    // 根据当前工作目录中的 `mode` 加载 .env 文件
-    // 设置第三个参数为 '' 来加载所有环境变量，而不管是否有 `VITE_` 前缀。
-    // https://cn.vitejs.dev/config/#environment-variables
-    // loadEnv(mode, process.cwd(), '')
-
-    // Thomas
-    // if (command === 'build' && mode === 'production')
-    // base: loadEnv('production', process.cwd()).VITE_APP_PATH,
-
-    // Ache
+    base: loadEnv(mode, process.cwd()).VITE_APP_PATH,
     // base: "diary",
-    // base: loadEnv(mode, process.cwd()).VITE_APP_PATH,
-    base: "/",
+    // base: "/",
     resolve: {
       alias: {
         "@": path.join(__dirname, "./src"),
