aa7e172fecd337a6bda5e558d909d4f4cb70bea4
zhang_gm@git <1424834523@qq.com>
2023-08-17 18:16:12 +0800

    fix: vercel接口转发

--- api/proxy.js
+++ api/proxy.js
@@ -1,12 +1,12 @@
-// api/proxy.js
 // 该服务为 vercel serve跨域处理
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = (req, res) => {
   let target = ''
+
   // 代理目标地址
   // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
-  // target 替换为你跨域请求的服务器 如： http://gmall-h5-api.atguigu.cn
+  // xxxxx 替换为你跨域请求的服务器 如： http://baidu.com
   if (req.url.startsWith('/ache')) {
     target = 'https://zhang.beer'
   }
@@ -15,9 +15,9 @@ module.exports = (req, res) => {
     target,
     changeOrigin: true,
     pathRewrite: {
-      // 通过路径重写，去除请求路径中的 `/api`
-      // 如果开启了,那么 /api/user/login 将被转发到 http://gmall-h5-api.atguigu.cn/user/login
-      //'^/api/': '/',
+      // 通过路径重写，去除请求路径中的 `/ache`
+      // 例如 /ache/login 将被转发到 https://zhang.beer/login
+      // '^/ache/': '/'
     },
   })(req, res)
 }
