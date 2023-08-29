2da4f2f5dd76648ab5c37fb849e221623ea25ea0
zhang_gm@git <1424834523@qq.com>
2023-08-29 16:14:39 +0800

    fix: vercel配置4

--- api/proxy.js
+++ api/proxy.js
@@ -1,23 +1,41 @@
-// 该服务为 vercel serve跨域处理
+// const { createProxyMiddleware } = require('http-proxy-middleware')
+
+// module.exports = (req, res) => {
+//   let target = ''
+
+//   if (req.url.startsWith('/ache')) {
+//     target = 'https://zhang.beer'
+//   }
+
+//   createProxyMiddleware({
+//     target,
+//     changeOrigin: true,
+//     pathRewrite: {
+//       // 通过路径重写，去除请求路径中的 `/ache`
+//       // 例如 /ache/login 将被转发到 https://zhang.beer/login
+//       // '^/ache/': '/'
+//     },
+//   })(req, res)
+// }
+
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = (req, res) => {
   let target = ''
 
-  // 代理目标地址
-  // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
-  // xxxxx 替换为你跨域请求的服务器 如： http://baidu.com
   if (req.url.startsWith('/ache')) {
     target = 'https://zhang.beer'
   }
-  // 创建代理对象并转发请求
+
+  // 获取客户端真实 IP 地址
+  const clientIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress
+
   createProxyMiddleware({
     target,
     changeOrigin: true,
-    pathRewrite: {
-      // 通过路径重写，去除请求路径中的 `/ache`
-      // 例如 /ache/login 将被转发到 https://zhang.beer/login
-      // '^/ache/': '/'
+    onProxyReq(proxyReq) {
+      // 在请求头中添加 X-Forwarded-For 字段
+      proxyReq.setHeader('X-Forwarded-For', clientIp)
     },
   })(req, res)
 }
